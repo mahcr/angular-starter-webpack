4d18f2960a7b55692a25af6689c17f84887d84a9
Mariano Alvarez <malvarezh92@gmail.com>
2016-12-21 21:47:57 -0600

    fix project

---
+++ config/webpack.config.common.js
@@ -0,0 +1,65 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var helpers = require('./helpers');
+
+module.exports = {
+  // will create 3 different files
+  entry: {
+    'polyfills': './src/polyfills.ts',
+    'vendor': './src/vendor.ts',
+    'app': './src/main.ts'
+  },
+
+  output: {
+      // to create each filw with the same of the key of entry
+      filename: '[name].js'
+  },
+
+  resolve: {
+    // make webpack understand imports
+    extensions: ['', '.ts', '.js'],
+    modulesDirectories: ['node_modules']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+      },
+      {
+        test: /\.html$/,
+        loader: 'html'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file?name=assets/[name].[hash].[ext]'
+      },
+      {
+        test: /\.css$/,
+        exclude: helpers.root('src', 'app'),
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
+      },
+      {
+        test: /\.css$/,
+        include: helpers.root('src', 'app'),
+        loader: 'raw'
+      }
+    ]
+  },
+
+  plugins: [
+    /**
+     * find shared dependecies and remove them from left to right
+     * if app -> share dependecies with vendor they will be removed from app
+     * */
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['app', 'vendor', 'polyfills']
+    }),
+
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
